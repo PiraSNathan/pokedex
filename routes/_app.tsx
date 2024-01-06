c7c0daf3428eff7e92a7e64f24f1e28d6ce892d5
pira <piranavi.satkunanathan@student.htw-berlin.de>
2024-01-06 22:13:21 +0100

    style: add font and css class for pokemon name

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/style.css" />
         <title>pokedex</title>
       </head>
       <body>
