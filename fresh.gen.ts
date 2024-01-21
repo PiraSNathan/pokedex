9c18b486c5f154f8a7806f23261b01384aaaa2c0
pira <piranavi.satkunanathan@student.htw-berlin.de>
2024-01-21 22:17:22 +0100

    refactor: migrate to tailwindcss

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,29 +2,30 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/pokemons/[id].tsx";
-import * as $5 from "./routes/pokemons/index.tsx";
-import * as $$0 from "./islands/navigate.tsx";
-import * as $$1 from "./islands/search.tsx";
+import * as $_404 from "./routes/_404.tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $api_joke from "./routes/api/joke.ts";
+import * as $index from "./routes/index.tsx";
+import * as $pokemons_id_ from "./routes/pokemons/[id].tsx";
+import * as $pokemons_index from "./routes/pokemons/index.tsx";
+import * as $pokemonContent from "./islands/pokemonContent.tsx";
+import * as $search from "./islands/search.tsx";
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_404.tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
-    "./routes/pokemons/[id].tsx": $4,
-    "./routes/pokemons/index.tsx": $5,
+    "./routes/_404.tsx": $_404,
+    "./routes/_app.tsx": $_app,
+    "./routes/api/joke.ts": $api_joke,
+    "./routes/index.tsx": $index,
+    "./routes/pokemons/[id].tsx": $pokemons_id_,
+    "./routes/pokemons/index.tsx": $pokemons_index,
   },
   islands: {
-    "./islands/navigate.tsx": $$0,
-    "./islands/search.tsx": $$1,
+    "./islands/pokemonContent.tsx": $pokemonContent,
+    "./islands/search.tsx": $search,
   },
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,17 @@
-import { type Config } from "npm:tailwindcss";
+import { type Config } from "tailwindcss";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: {
+      boxShadow: {
+        tab: "0 0px 30px -2px rgb(0,0,0, 0.5)",
+      },
+      fontFamily: {
+        pokemonNameB: ["Pixelify Sans", "sans-serif"],
+      },
+    },
+  },
 } satisfies Config;
