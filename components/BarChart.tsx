060820a4c3bcc24656c8b00702785ede000419c6
pira <piranavi.satkunanathan@student.htw-berlin.de>
2024-02-17 00:21:19 +0100

    add bar chart component

---
+++ components/BarChart.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  value: number;
+  colour?: string;
+}
+
+const BarChar = (props: Props) => {
+  return (
+    <div className="h-4 border-[#6f6d708f] border border-1 rounded-md w-[200px]">
+      <div
+        style={{
+          width: `${props.value > 100 ? 100 : props.value}%`,
+          backgroundColor: `${props.colour}`,
+        }}
+        className={`h-full rounded-md ${!props.colour && "bg-blue-500"}`}
+      >
+      </div>
+    </div>
+  );
+};
+
+export default BarChar;
