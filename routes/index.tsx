a732154310638138192c6fe51bdd62e8e295a8ee
pira <piranavi.satkunanathan@student.htw-berlin.de>
2024-03-27 17:50:18 +0100

    refactor: add welcome text

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,26 +1,21 @@
 import { useSignal } from "@preact/signals";
 
 export default function Home() {
-  const count = useSignal(6);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div
-        class="max-w-screen-md mx-auto flex flex-col items-center justify-center bg-[url('/logo.svg')]"
-        style={{ backgroundImage: `url('/logo.svg')` }}
-      >
-        {
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-        }
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+    <div className="flex min-h-screen justify-center items-center content-center ">
+      <div class="px-4 py-8 mx-auto bg-[#3D6FBA] w-3/4 md:w-1/5 rounded-xl p-2 space-y-2">
+        <h1 class="text-4xl font-bold text-white">Welcome!</h1>
+        <p class="text-lg text-white">
+          In this Pokedex you will find detailed information on the first 1000
+          Pokemons. You can browse through the Pokemons, search for a specific
+          Pokemon and filter them by type. Click on any Pokemon to get more
+          detailed information about it. With the navigation buttons on the
+          bottom right you can check out the previous/next Pokemon.
+        </p>
+        <p>
+          <a href={`/pokemons`} className="underline text-white">
+            Let's checkout the Pokemons!
+          </a>
         </p>
       </div>
     </div>
